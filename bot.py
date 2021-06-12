bee013b475e2d1ea49c205e40c54307d81f52bb9
lego0623 <63096280+lego0623@users.noreply.github.com>
2021-06-13 01:30:01 +0900

    .

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import asyncio,discord,os
 from discord import message
+from discord import channel
 from discord.ext import commands
 from discord.utils import deprecated
 import requests
@@ -14,8 +15,98 @@ mafia_gameing = False
 mafia_game_count = 0
 end_bind_ready = False
 end_binding = False
-end_bind_count = 0
+end_bind_user = ""
+end_bind_end = ""
+end_bind_list = []
+end_bind_time = False
+end_bind_score = 0
 alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," ",".","?","!","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"]
+with open('words.txt', 'rt', encoding='UTF8') as f:
+    data = f.read()
+file_list = data.splitlines()
+
+@bot.event
+async def on_message(message):
+    global end_bind_end
+    global end_bind_list
+    global end_bind_ready
+    global end_bind_user
+    global end_binding
+    global end_bind_time
+    global end_bind_score
+    if(message.content.startswith('!!끝말잇기_시작') and not end_binding):
+        msg = await message.channel.send("끝말잇기가 5초 뒤에 시작합니다.")
+        for i in range(0, 4):
+            time.sleep(1)
+            await msg.edit(content='끝말잇기가 ' + str(4-i) + '초 뒤에 시작합니다.')
+        time.sleep(1)
+        await msg.edit(content='끝말잇기가 시작합니다.')
+        end_binding = True
+        end_bind_time = True
+        end_bind_user = str(message.author)
+        first_word = file_list[random.randrange(0, len(file_list))]
+        # random.range()
+        await message.channel.send("제시어: " + first_word + "")
+        await message.channel.send("" + str(message.author) + "님 시작 단어(" + first_word[len(first_word)-1] + ")") 
+        end_bind_end = first_word
+    if(message.content.startswith("!!ㅇ")):
+        print(str(message.content))
+        print(end_bind_end)
+        print(str(message.content[len(str(message.content))-1]))
+        aghdg = []
+        if(end_binding):
+            if(str(message.content) == "!!ㅇ !포기!"):
+                if(end_bind_time):
+                    await message.channel.send(content='당신이 졌군요 /' + str(end_bind_score) + '점/패배')
+                    end_bind_ready = False
+                    end_binding = False
+                    end_bind_user = ""
+                    end_bind_end = ""
+                    end_bind_list = []
+                    end_bind_time = False
+                    end_bind_score = 0
+            else:
+                if(str(message.content[4]) == end_bind_end[len(end_bind_end)-1] and str(message.author) == end_bind_user):
+                    print("asdfds")
+                    if(message.content[4:len(message.content)] in file_list and not message.content[4:len(message.content)] in end_bind_list):
+                        end_bind_time = False
+                        end_bind_score += 1
+                        for ggg in range(0, len(file_list)):
+                            if(message.content[len(message.content)-1] == file_list[ggg][0]):
+                                aghdg.append(file_list[ggg])
+                        end_bind_end = aghdg[random.randrange(0, len(aghdg))]
+                        end_bind_list.append(end_bind_end)
+                        try:
+                            await message.channel.send("" + end_bind_end + ", " + str(message.author) + "님 시작 단어(" + end_bind_end[len(end_bind_end)-1] + ")") 
+                            print(end_bind_end)
+                            msg = await message.channel.send("10초 남음")
+                            end_bind_time = True
+                            for ijk in range(0, 10):
+                                if(end_bind_time):
+                                    time.sleep(1)
+                                if(end_bind_time):
+                                    await msg.edit(content='' + str(10-ijk) + '초 남음')
+                            if(end_bind_time):
+                                time.sleep(1)
+                            if(end_bind_time):
+                                await msg.edit(content='당신이 졌군요 /' + str(end_bind_score) + '점/패배')
+                                end_bind_ready = False
+                                end_binding = False
+                                end_bind_user = ""
+                                end_bind_end = ""
+                                end_bind_list = []
+                                end_bind_time = False
+                                end_bind_score = 0      
+                        except:
+                            await message.msg.edit("워우...제가 졌군요... /" + str(end_bind_score) + "점/승리")
+                            end_bind_ready = False
+                            end_binding = False
+                            end_bind_user = ""
+                            end_bind_end = ""
+                            end_bind_list = []
+                            end_bind_time = False
+                            end_bind_score = 0  
+
 
 @bot.event
 async def on_ready():
@@ -51,19 +142,21 @@ async def 도움_명령어(ctx):
     embed.add_field(name=f"-!!도움_명령어",value=f"명령어 목록", inline=False)
     await ctx.send(embed=embed)
 
-@bot.command()
-async def 끝말잇기_시작(ctx):
-    msg = await ctx.send("끝말잇기가 5초 뒤에 시작합니다.")
-    for i in range(0, 4):
-        time.sleep(1)
-        await msg.edit(content='끝말잇기가 ' + str(4-i) + '초 뒤에 시작합니다.')
-    time.sleep(1)
-    await msg.edit(content='끝말잇기가 시작합니다.')
-    file = open('words.txt', 'rb')
-    words = file.read()
-    print(words)
-    # random.range()
-    await ctx.send("제시어: ")
+# @bot.command()
+# async def 끝말잇기_시작(ctx):
+#     msg = await ctx.send("끝말잇기가 5초 뒤에 시작합니다.")
+#     for i in range(0, 4):
+#         time.sleep(1)
+#         await msg.edit(content='끝말잇기가 ' + str(4-i) + '초 뒤에 시작합니다.')
+#     time.sleep(1)
+#     await msg.edit(content='끝말잇기가 시작합니다.')
+#     with open('words.txt', 'rt', encoding='UTF8') as f:
+#         data = f.read()
+#     file_list = data.splitlines()
+#     first_word = file_list[random.randrange(0, len(file_list))]
+#     # random.range()
+#     await ctx.send("제시어: " + first_word + "")
+#     await ctx.send("" + arg.author + "님 시작 단어(" + first_word[len(first_word)-1] + "") 
     
 
 
