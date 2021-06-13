cee80af2f8284af3db8b84e861d742a054ba1083
lego0623 <63096280+lego0623@users.noreply.github.com>
2021-06-13 17:13:29 +0900

    .

--- bot.py
+++ bot.py
@@ -29,9 +29,6 @@ if(True): # 숫자야구 변수
     num_base_home = []
 
 alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," ",".","?","!","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"]
-with open('words.txt', 'rt', encoding='UTF8') as f:
-    data = f.read()
-file_list = data.splitlines()
 
 @bot.event
 async def on_ready():
@@ -96,11 +93,115 @@ async def 도움_명령어(ctx):
     embed.add_field(name=f"-!!도움",value=f"딱히 쓸모없음", inline=False)
     embed.add_field(name=f"-!!날씨",value=f"날씨를 알려줌(안됨)", inline=False)
     embed.add_field(name=f"-!!도움_명령어",value=f"명령어 목록", inline=False)
+    embed.add_field(name=f"-!!도움_이진수",value=f"숫자야구 규칙", inline=False)
     embed.add_field(name=f"-!!끝말잇기_도움",value=f"끝말잇기 사용방법", inline=False)
     embed.add_field(name=f"-!!끝말잇기_게임방법",value=f"끝말잇기 규칙", inline=False)
     embed.add_field(name=f"-!!숫자야구_도움",value=f"숫자야구 사용방법", inline=False)
     embed.add_field(name=f"-!!숫자야구_게임방법",value=f"숫자야구 규칙", inline=False)
     await ctx.send(embed=embed)
+
+@bot.event
+async def on_ready():
+    print("마이야히 마이야하")
+
+@bot.command()
+async def 도움_이진수(ctx):
+    embed = discord.Embed(title=f"사용방법", descriotion=f"이진봇", Color=0xf3bb76)
+    embed.add_field(name=f"-이진수를 십진수로",value=f"이진수_십진수 (이진수)", inline=False)
+    embed.add_field(name=f"-십진수를 이진수로",value=f"십진수_이진수 (이진수)", inline=False)
+    embed.add_field(name=f"-이진수를 영어로",value=f"이진수_영어 (이진수)", inline=False)
+    embed.add_field(name=f"-영어를 이진수로",value=f"영어_이진수 (영어)", inline=False)
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def 이진수_십진수(ctx, text):
+    a = text
+    b = 0
+    for i in range(0, len(a)):
+        if(a[i] == "1" and b == 0):
+            b = 1
+        elif(a[i] == "1"):
+            b = b*2+1
+        elif(a[i] == "0"):
+            b = b*2
+
+    await ctx.send(b) 
+
+@bot.command()
+async def 십진수_이진수(ctx, text):
+    b = int(text)
+    a = 0
+    c = ""
+
+    while(b!=1):
+        a += 1
+        if(b%2 == 1):
+            b = (b-1)/2
+            c = "1" + c
+        else:
+            b = b/2
+            c = "0" + c
+
+    c = "1" + c
+    await ctx.send(c)
+
+@bot.command()
+async def 이진수_영어(ctx, text):
+    a = text
+    b = 0
+    c = ""
+    d = int(len(a)/5)
+
+    for i in range(0, d):
+        b = 0
+        for j in range(i*7, i*7+7):
+            if(a[j] == "1"):
+                b = b*2+1
+            elif(a[j] == "0"):
+                b = b*2
+        c = c+alphabet[b-1]
+
+
+    print(c)
+    await ctx.send(c)
+
+@bot.command()
+async def 영어_이진수(ctx, *, text):
+    p = []
+
+    o = text
+
+    for i in range(0, len(o)):
+        for j in range(0, len(alphabet)):
+            if(alphabet[j] == o[i]):
+                p.append(j+1)
+
+    a = 0
+    c = ""
+    result = ""
+
+    for k in range(0, len(p)):
+        b=p[k]
+        a=0
+        c=""
+        while(b!=1):
+            a += 1
+            if(b%2 == 1):
+                b = (b-1)/2
+                c = "1" + c
+            else:
+                b = b/2
+                c = "0" + c
+
+        c = "1" + c
+        if(len(c) != 5):
+            for ii in range(0, 5-len(c)):
+                c = "0" + c
+
+        result = result+c
+    await ctx.send(result)
+
+
 
 # @bot.command()
 # async def 끝말잇기_시작(ctx):
